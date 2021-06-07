d46fe2310db67eb42ba7bc92fce06c555320071d
murthyvsap <murthy.v@sap.com>
2021-06-07 15:30:36 +0000

    business validation for mobile and email added

--- srv/eventmanager-service.js
+++ srv/eventmanager-service.js
@@ -16,11 +16,46 @@ module.exports = cds.service.impl(srv => {
         }
     })
 
+        function validateEmail(email) {
+        var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (email.match(regexEmail)) {
+            return true; 
+        } else {
+            return false; 
+        }
+    }
+
+    function validatePhone(phone) {
+        var regexPhone = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+
+        if (phone.match(regexPhone)) {
+            return true; 
+        } else {
+            return false; 
+        }
+    }
   // reject creation of participants in case there are no available slots
    srv.before("CREATE", "Participants", async req => {
         try {
            const { EventManager } = cds.services;
             const { Events , Participants } = cds.entities("sap.cae.eventmanagement");
+
+            // Check the validity of email before creation of participant
+            if( req.data.email !== 'undefined' && req.data.email ) {
+                if ( validateEmail(req.data.email ) === false ) {
+                    // invalid email
+                    req.error("Invalid Email ID: "+req.data.email, +"please enter valid email ");  
+                }
+            }
+            // Check the validity of phone  before creation of participant
+            if( req.data.mobileNumber !== 'undefined' && req.data.mobileNumber ) {
+                if ( validatePhone(req.data.mobileNumber ) === false ) {
+                    // invalid phone number
+                    req.error("Invalid Mobile Number: "+req.data.mobileNumber, +"please enter valid mobile number ");  
+                }
+            }
+
+            // Check the status of the event status before adding participant 
             let eventIDs = [];
             eventIDs.push(req.data.parent_ID); 
             const events = await EventManager.read(Events).where("ID in", eventIDs);   
@@ -44,6 +79,39 @@ module.exports = cds.service.impl(srv => {
             {
               req.error("Cannot Add Participants : Event is not released , try after publishing the event :-) ");  
             }
+        } catch (error) {
+            req.error(error);
+        }
+    })
+
+    // check the validity of email
+   srv.after("UPDATE", "Participants", async req => {
+        try {
+                      
+            const { EventManager } = cds.services;
+            const { Participants } = cds.entities("sap.cae.eventmanagement");
+            let eventIDs = [];
+            eventIDs.push(req.ID);
+            if( req.email !== 'undefined' && req.email ) {
+                // Check the validity of email after update of participant
+                if ( validateEmail(req.email ) === false ) {
+                    // invalid email
+                    req.error("Invalid Email ID: "+req.email, +"please enter valid email ");  
+                }else{
+                    //update cancellation status of event 
+                    const updateEvent = await UPDATE(Participants).set({email: req.email}).where("ID in", eventIDs);
+                }
+            }
+             if( req.mobileNumber !== 'undefined' && req.mobileNumber ) {
+                // Check the validity of Phone after update of participant
+                if ( validatePhone(req.mobileNumber ) === false ) {
+                    // invalid phone
+                    req.error("Invalid Email ID: "+req.email, +"please enter valid email ");  
+                }else{
+                    //update phone
+                    const updateEvent = await UPDATE(Participants).set({email: req.phone}).where("ID in", eventIDs);
+                }
+             }
         } catch (error) {
             req.error(error);
         }
@@ -94,6 +162,8 @@ module.exports = cds.service.impl(srv => {
             req.error(error);
         }
     })
+
+
         /* dont allow completed events to be deleted */
     srv.on("DELETE", "Events", async (req, next) => {
          const { Events } = srv.entities;
